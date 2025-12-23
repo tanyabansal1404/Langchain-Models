ec38a05671ec4ed67ad2012bcd3f5559088bd84e
tanyabansal1404 <tanyabansal1404@gmail.com>
2025-12-23 14:05:54 +0530

    Final Modifications

--- 3.EmbeddingModels/2_embedding_openai_docs.py
+++ 3.EmbeddingModels/2_embedding_openai_docs.py
@@ -6,9 +6,9 @@ load_dotenv()
 embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
 
 documents = [
-    "Delhi is the capital of India",
-    "Shimla is the capital of Himachal Pradesh",
-    "Paris is the capital of France"
+    "Delhi is the capital of India.",
+    "Shimla is the capital of Himachal Pradesh.",
+    "Paris is the capital of France."
 ]
 
 result = embedding.embed_documents(documents)
